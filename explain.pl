410cd21cd90ce0af3409f46ebba9d00829aca43a
JanPro <janpro@janpro.dev>
2022-05-21 16:57:57 +0200

    Begin implementation, add example input

--- explain.pl
+++ explain.pl
@@ -1,1 +1,80 @@
-:- use_module(library(csv))
+% #!/usr/bin/swipl 
+% :-initialization(main,main).
+:- use_module(library(csv)).
+:- use_module(library(clpfd)).
+
+% group: group(Teacher, Students, Concept)
+% round(Groups) groups is a list of groups
+% todo use arguments to get the file
+main:-write("hi").
+
+numeric("N",0).
+numeric("MHE",1).
+numeric("HE",2).
+numeric("CE",3).
+
+group_has_two_students(Group) :-
+    Group = group(Teacher, Students,_),
+    length(Students, 2).
+
+group_has_between_1_and_3_students(Group) :- 
+    Group = group(Teacher, Students, _),
+    length(Students, N),
+    N #> 0,
+    N #< 4.
+everyone_once(Groups,People) :-
+     % every person from People is in exactly one group
+    append(Groups,GroupsPeople), equal(GroupsPeople,People) , !.
+
+
+isSubset([],_).
+isSubset([H|T],Y):-
+    member(H,Y),
+    select(H,Y,Z),
+    isSubset(T,Z).
+equal(X,Y):-
+    isSubset(X,Y),
+    isSubset(Y,X).
+
+% ["name", "Concept1", "Concept2", "Concept3"]
+% ["Jan", "MHE", "HE", "CE"]
+% convert this to [relation("Jan","Concept1","MHE"), relation("Jan","Concept2","HE"), relation("Jan","Concept3","CE")]
+
+pair(A, B, A : B).
+to_relation(Name,Concept,Action, relation(Name,Concept,Action)).
+
+rowlist_to_rels([_|HeaderL],[Name|RowL],Rels):-
+    maplist(to_relation(Name),HeaderL,RowL,Rels)
+    % append(Name,Concept,Action)
+    
+    .
+
+rows2rels(_,[],[]).
+rows2rels(Header,[],[]).
+rows2rels(Header,Rows,Rels):-
+    row_to_list(Header,HeaderL),
+    rows_to_lists(Rows,RowsL),
+    maplist(rowlist_to_rels(HeaderL),RowsL,RelsLL),
+    flatten(RelsLL,Rels).
+    
+
+rels2groups(Rels,Groups).
+outheader(row("concept","teacher","student1","student2","student3")).
+
+rels2rounds(_,_).
+rounds2rows(_,[row("ahoj","hey")]).
+
+rows_to_lists(Rows, Lists):-
+  maplist(row_to_list, Rows, Lists).
+
+row_to_list(Row, List):-
+  Row =.. [row|List].
+
+explain(InPath,OutPath) :-
+     csv_read_file(InPath,[Header|InRows]),
+     rows2rels(Header,InRows,RowsRel),
+     rels2rounds(RowsRel,Rounds),
+     rounds2rows(Rounds,OutRows),
+     outheader(OutHeader),
+     csv_write_file(OutPath, [OutHeader|OutRows])
+     .
--- sanitize.py
+++ sanitize.py
@@ -2,28 +2,28 @@ import json
 import sys,os
 import pandas as pd
 def remove_first_col(csv_path,output_path):
-    df = pd.read_csv(csv_path)
-    first_column = df.columns[0]
-    df = df.drop([first_column], axis=1)
-    df.to_csv(output_path, index=False)
+    with open(csv_path) as i:
+        with open(output_path,'w') as o:
+            for line in i.readlines():
+                line = line.split(',')
+                o.write(','.join(line[1:]))
 
 def sanitize(translate, input_path, output):
     temp_path = '__temp__csvthingy.csv'
     remove_first_col(input_path,temp_path)
     with open(temp_path) as i:
-        text = i.read()
-        transdict = json.load(translate)
-        for (k,v) in transdict.items():
-            text = text.replace(v,k)
-        with open(output,'w') as o:
-            o.write(text)
+        with open(translate) as t:
+            text = i.read()
+            transdict = json.load(t)
+            for (k,v) in transdict.items():
+                text = text.replace(v,k)
+            with open(output,'w') as o:
+                o.write(text)
     os.remove(temp_path) 
+
 if __name__ == "__main__":
-    print('todo test this')
-    print('make this at leas functional?')
-    print('todo consider replacing pandas with pure python')
     if len(sys.argv) ==4:
         _, translate, input_path, output_path = sys.argv
         sanitize(translate,input_path,output_path)
     else:
-        print('invalid number of args')
+        print('usage: python3 sanitize.py [translate.json] [input.csv] [output.csv]')
