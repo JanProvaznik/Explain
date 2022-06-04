6279b7883ee79ef2143e10e08efb8690e8ccb1d9
JanPro <janpro@janpro.dev>
2022-06-04 21:04:44 +0200

    Bugfix, update documentation, big refactor

--- README.md
+++ README.md
@@ -1,13 +1,48 @@
-# Explain
-Group creation automation for [Explaining concepts activity](https://forum.effectivealtruism.org/posts/r8Qv7QHjJyafmiLnp/#Explaining_Concepts__9_30_10_40_). Used as an icebreaker and helping to establish a shared context in a group that wants to have a deep discussion. Intended for creating groups with a ratio of 1 explainer : 2 students in three rounds for 15-50 people and 8-15 concepts. 
+# Explain (probably in a development dead end)
+Group creation automation for [Explaining concepts activity](https://forum.effectivealtruism.org/posts/r8Qv7QHjJyafmiLnp/#Explaining_Concepts__9_30_10_40_). Used as an icebreaker and helping to establish a shared context in a group that wants to have a deep discussion. Intended for creating groups with a ratio of 1 explainer : 2 students in three rounds for ~~15-50~~ 9 people and 8-15 concepts. 
 
-### Workflow (not working yet, might change so that everything is done by prolog):
+## Documentation
+
+The main predicate is `explain(+InPath,+OutPath,-OutputRows)` which loads csv from *InPath*, dynamically establishes relations based on its content, generates solutions using `rounds9`, and writes the current solution to *OutPath*.
+
+The `rounds9(?Rounds)` predicate generates a list of 3 rounds for 9 people using  *clpfd* library integer constraints. It uses first-fail heuristic in the `labeling([ff],Vs]` predicate and global constraints: `global_cardinality(Vs,Pairs]` and `all_distinct(Vs)`.
+
+Other code is mainly used for converting input into integers and then back to strings for the output.
+
+## Development journal
+- March
+I was inspired to do this project by hearing a friend complain how long it took him to create reasonable groups manually for the activity described above.
+
+- April
+Ambitious project proposal.
+
+- 2022-05-21
+Accompanying python script. For transforming form output to desired file. Discovered the `=..` operator for converting functors to lists. Basic input handling into lists of relations.
+
+- 2022-05-28
+Generating groups and then selecting groups to form a solution is not viable.
+9 People ~1000 groups, 18 People ~20k groups, 36 People ~400k groups, 54 People ~2M groups
+I tried using a trick -> divide people into subgroups of 9 people and solve for them separately.
+If we generate the rounds naively from groups in the case of 9 people, it's still doing having 1000 choose 3 ~ 166 million options, that's too much. 
+
+- 2022-05-30
+`dynamic` predicate is what I needed from the beginning! Checking for membership in relationship list is strange and having relationships be predicates is significantly faster. Now I can use integer constraints with ease.
+
+- 2022-06-03
+I tried reducing the search space by constraining in which order the students in one group, and teachers in one round can be. It helped, but still when a solution is impossible the program usually hangs instead of outputting `false`.
+
+- 2022-06-04
+The original plan was to have a variable number of students in each group, but that looks like a very bad idea now, that it barely computes any solutions with fixed size groups.
+
+On artificial data (such as running `explain("example9.csv","output.csv",Output).`) it performs barely OK, but for the intended use with many edge cases (like a person that can only explain or only wants to have explained) it's far too slow. I expected that with all the global constraints and first-fail heuristic it would be usable, but now I'm not sure how to save this. In retrospect, I was way too optimistic and should have done more calculations, whether the project is feasible.
+
+
+
+## ~~Workflow~~ (does not work):
 1. Create a google form in this template: https://docs.google.com/forms/d/e/1FAIpQLSft_PXbcs4GAv8Hp12ibl69OBMkb2hgdeBdxDKwaEFncDUdyw/viewform?usp=sf_link
 2. If you use other names for options create your own `translate.json` that has keys as your options for each value: CE = Can explain, HE = Wants to have explained, MHE = Maybe wants to have explained, N = Does not want to explain nor have explained 
 3. link it to google sheets
 4. download the Gsheet as csv
-5. use the explain python script to sanitize the input `python3 sanitize.py translate.json /path/to/exported.csv /path/to/sanitized.csv`
-6. run `explain.pl`
-7. Groups are created in a new csv file!
-
-## Documentation
+5. use the `sanitize.py` script to sanitize the input `python3 sanitize.py translate.json /path/to/exported.csv /path/to/sanitized.csv`
+6. run `swipl explain.pl` and then inside `swipl` run `explain("input.csv","output.csv",Rows).`
+7. Rounds are outputted to the csv file!
--- explain.pl
+++ explain.pl
@@ -1,65 +1,35 @@
-% #!/usr/bin/swipl 
-% :-initialization(main,main).
 :- use_module(library(csv)).
 :- use_module(library(clpfd)).
 :- use_module(library(apply)).
 
 :- dynamic relation/3.
 
-% Notes: generating by groups and then choosing groups to form a solution does not work
-% group: group(Teacher, Students, Concept)
-% 9 People ~1000 groups 18 People ~20k groups, 36 People ~400k groups, 54 People ~2M groups
-% that means generating all groups seems very bad...
-% trick -> divide into subgroups of 9 people and solve for them separately!
-
-% even if we did that selecting round is still doing 1000 choose 3 which is 166 million options
-
 % region: internal numeric representation of relations
-
 range(Lo,Hi,Range):-findall(N,between(Lo,Hi,N),Range).
 pair(A, B, A : B).
 % can be used both for People and Concepts, dict has structure Number : Concept
-list_to_dict(X, Dict) :-
-    length(X, Len),
-      range(1,Len,Y),
-      maplist(pair,Y,X,Dict).
+list_to_dict(L, Dict) :-
+    length(L, Len),
+    range(1,Len,Nums),
+    maplist(pair,Nums,L,Dict).
 
-
-relation_to_numeric(PeopleDict,ConceptDict,relation(Person,Concept,Action),relation(PersonN,ConceptN,ActionN)):-
-    member(PersonN:Person,PeopleDict),
-    member(ConceptN:Concept,ConceptDict),
-    numeric(Action,ActionN)
-    .
-
+% numbers don't have meaning
 numeric('N',0).
 numeric('MHE',1).
 numeric('HE',3).
 numeric('CE',-6).
-
+% better usability than relations: it's possible to maplist these
 can_explain(Teacher,Concept):-relation(Teacher,Concept,-6).
 really_wants_have_explained(Student,Concept):-relation(Student,Concept,3).
 maybe_wants_have_explained(Student,Concept):-relation(Student,Concept,1).
 wants_have_explained(Student,Concept):- really_wants_have_explained(Student,Concept) ; maybe_wants_have_explained(Student,Concept).
-rev_wants_he(Concept,Student):-wants_have_explained(Student,Concept).
+
+relation_to_numeric(PeopleDict,ConceptDict,relation(Person,Concept,Action),relation(PersonN,ConceptN,ActionN)):-
+    member(PersonN:Person,PeopleDict),
+    member(ConceptN:Concept,ConceptDict),
+    numeric(Action,ActionN).
+
 % endregion
-
-
-%???
-
-
-% group_has_two_students(group(_,Students,_)) :-
-%     length(Students, 2).
-
-% group_has_between_1_and_3_students(Group) :- 
-%     Group = group(_, Students, _),
-%     length(Students, N),
-%     N #> 0,
-%     N #< 4.
-
-% ['name', 'Concept1', 'Concept2', 'Concept3']
-% ['Jan', 'MHE', 'HE', 'CE']
-% convert this to [relation("Jan","Concept1","MHE"), relation("Jan","Concept2","HE"), relation("Jan","Concept3","CE")]
-
 
 % region: IO
 % generate a header
@@ -68,38 +38,31 @@ outheader(row("concept","teacher","student1","student2","student3")).
 not_interested(relation(_,_,X)):- 
     numeric('N',Z),
     X #= Z.
-is_teacher(relation(_,_,X)):-
-    numeric('CE',Z),
-    X #= Z.
-% putting into a functor
+% creating a relation functor 
 to_relation(Name,Concept,Action, relation(Name,Concept,Action)).
 
 % list of rows -> list of relation functors
 rowlist_to_rels([_|HeaderL],[Name|RowL],Rels):-
     maplist(to_relation(Name),HeaderL,RowL,Rels).
 
-
+% converts inner rows of csv to numeric relation functors
 rows_to_rels(PeopleDict,ConceptDict,Header,Rows,Rels):-
     row_to_list(Header,HeaderL),
     rows_to_lists(Rows,RowsL),
     maplist(rowlist_to_rels(HeaderL),RowsL,RelsLL),
     flatten(RelsLL,RelsS),
-    maplist(relation_to_numeric(PeopleDict,ConceptDict),RelsS,Rels)
-       . 
+    maplist(relation_to_numeric(PeopleDict,ConceptDict),RelsS,Rels).
 
-
-member_of(X,Y):-member(Y,X).
 list_of_numbers_to_people(_,[],[]).
 list_of_numbers_to_people(PeopleDict,[N|Numbers],[P|People]):-
     member(N:P,PeopleDict),
     list_of_numbers_to_people(PeopleDict,Numbers,People).
+
 % converts an integer group to a list of Strings
 group_to_string_list(PeopleDict,ConceptDict,group(Teacher,Students,Concept),[ConceptS,TeacherS|StudentsS]):-
-member(Teacher:TeacherS, PeopleDict),member(Concept:ConceptS,ConceptDict),list_of_numbers_to_people(PeopleDict,Students,StudentsS).
+    member(Teacher:TeacherS, PeopleDict),member(Concept:ConceptS,ConceptDict),list_of_numbers_to_people(PeopleDict,Students,StudentsS).
 
-
-
-% rounds contain integer groups, we need to convert them to string groups
+% rounds contain integer groups, need to convert them to string groups for output
 rounds_to_rows(PeopleDict,ConceptDict,Rounds,Rows):-
     Rounds = [R1,R2,R3],
     maplist(group_to_string_list(PeopleDict,ConceptDict),R1,R1L),
@@ -116,25 +79,26 @@ rows_to_lists(Rows, Lists):-
   maplist(row_to_list, Rows, Lists).
 
 row_to_list(Row, List):-
-  Row =.. [row|List].
+    Row =.. [row|List].
+
 list_to_row(List,Row):-row_to_list(Row,List).
 
 filter_does_not_want(Relations,FilteredRelations):-
     exclude(not_interested,Relations,FilteredRelations).
 % endregion: IO
 
-% split_teacher_student(Relations,TeacherRelations,StudentRelations):-
-    % partition(is_teacher, Relations, TeacherRelations, StudentRelations).
-% I want the student to exist in the relation with the concept
-
-pair_compatible((A,B),(C,D)):-
-    (A #= C #/\ B #= D) #\/ (A #= C #==> B #\= C).
+% one student in two rounds has to have a different concept
+pair_compatible((S1,C1),(S2,C2)):-
+    (S1 #= S2) #==> (C1 #\= C2).
     
 compatible_with(Exs,X):-
-    maplist(pair_compatible(X),Exs).
+    select(X, Exs, Rest),% don't compare with the same variable
+    maplist(pair_compatible(X),Rest).
 
-pairs_compatible(Exs):-
+no_one_twice(Exs):-
     maplist(compatible_with(Exs),Exs).
+
+pair3(X,X-3).
 % the simple case
 rounds9(ConceptCount,[Round1,Round2,Round3]):-
     Round1 = [  group(T0,[S00,S01],C0),
@@ -148,27 +112,32 @@ rounds9(ConceptCount,[Round1,Round2,Round3]):-
     Round3 = [  group(T6,[S60,S61],C6),
                 group(T7,[S70,S71],C7),
                 group(T8,[S80,S81],C8)],
-    % each round has to contain one person once
-    [T0,T1,T2,T3,T4,T5,T6,T7,T8] ins 1..9,
-    [C0,C1,C2,C3,C4,C5,C6,C7,C8] ins 1..ConceptCount,
-    [S00,S01,S10,S11,S20,S21,S30,S31,S40,S41,S50,S51,S60,S61,S70,S71,S80,S81] ins 1..9,
-    T0#<T1,T1#<T2,%T0#<T2,
-    T3#<T4,T4#<T5,%T3#<T5,
-    T6#<T7,T7#<T8,%T6#<T8,
+    AllConcepts = [C0,C1,C2,C3,C4,C5,C6,C7,C8],
+    AllTeachers = [T0,T1,T2,T3,T4,T5,T6,T7,T8],
+    AllFirstStudents = [S00,S10,S20,S30,S40,S50,S60,S70,S80],
+    AllSecondStudents = [S01,S11,S21,S31,S41,S51,S61,S71,S81],
+    AllPeople = [T0,T1,T2,T3,T4,T5,T6,T7,T8,S00,S01,S10,S11,S20,S21,S30,S31,S40,S41,S50,S51,S60,S61,S70,S71,S80,S81],
+    append(AllConcepts,AllPeople,Everything),
+    % restricts the domain as well as enforcing each person is assigned thrice
+    range(1,9,Range),
+    maplist(pair3,Range,Cardinalities),
+    global_cardinality(AllPeople,Cardinalities),
+    % concepts are in the domain of 1..ConceptCount
+    AllConcepts ins 1..ConceptCount,
+    % teachers in a round are differnt and ordered to reduce search space
+    T0#<T1,T1#<T2,
+    T3#<T4,T4#<T5,
+    T6#<T7,T7#<T8,
 
-    S00#<S01,S10#<S11,S20#<S21,
-    S30#<S31,S40#<S41,S50#<S51,
-    S60#<S61,S70#<S71,S80#<S81,
+    % ordering of students reduces search space
+    maplist(#<,AllFirstStudents,AllSecondStudents),
 
-
-    % all_distinct([T0,T1,T2]),               
-    % all_distinct([T3,T4,T5]),
-    % all_distinct([T6,T7,T8]),
-    
+    % students in rounds are different
     all_distinct([S00,S01,S10,S11,S20,S21]),
     all_distinct([S30,S31,S40,S41,S50,S51]),
     all_distinct([S60,S61,S70,S71,S80,S81]),
 
+    % teacher in one group can't be a student in the same round
     all_distinct([T0,S10,S11,S20,S21]),
     all_distinct([T1,S00,S01,S20,S21]),
     all_distinct([T2,S00,S01,S10,S11]),
@@ -179,56 +148,49 @@ rounds9(ConceptCount,[Round1,Round2,Round3]):-
 
     all_distinct([T6,S70,S71,S80,S81]),
     all_distinct([T7,S60,S61,S80,S81]),
-    all_distinct([T8,S60,S61,S70,S71])
- ,
-    % if a person is explained a concept C, then they should not be explained the same concept C again
+    all_distinct([T8,S60,S61,S70,S71]),
 
-    Exs= [(S00,C0),(S01,C0),(S10,C1),(S11,C1),(S20,C2),(S21,C2),(S30,C3),(S31,C3),(S40,C4),(S41,C4),(S50,C5),(S51,C5),(S60,C6),(S61,C6),(S70,C7),(S71,C7),(S80,C8),(S81,C8)],
-    pairs_compatible(Exs),
-    
-    
-   % noone should explain two times in a row and ideally noone should explain twice
-    (all_distinct([T0,T1,T2,T3,T4,T5,T6,T7,T8]); 
+    % if a person is explained a concept C, then they should not be explained the same concept C again
+    Explanations = [(S00,C0),(S01,C0),(S10,C1),(S11,C1),(S20,C2),(S21,C2),(S30,C3),(S31,C3),(S40,C4),(S41,C4),(S50,C5),(S51,C5),(S60,C6),(S61,C6),(S70,C7),(S71,C7),(S80,C8),(S81,C8)],
+    no_one_twice(Explanations),
+
+    % ideally no one should explain twice, but there is 'or' if it's inevitable
+    (all_distinct(AllTeachers); 
     all_distinct([T0,T1,T2,T3,T4,T5,T6,T7]);
     all_distinct([T0,T1,T2,T3,T4,T5,T6]);
-    all_distinct([T0,T1,T2,T3,T4,T5]),
-    all_distinct([T0,T1,T2,T3,T4]),
+    all_distinct([T0,T1,T2,T3,T4,T5]);
+    all_distinct([T0,T1,T2,T3,T4]);
     all_distinct([T0,T1,T2,T3])
     ),
 
-    maplist(can_explain, [T0,T1,T2,T3,T4,T5,T6,T7,T8],[C0,C1,C2,C3,C4,C5,C6,C7,C8])           ,
-    maplist(wants_have_explained, [S00,S10,S20,S30,S40,S50,S60,S70,S80],[C0,C1,C2,C3,C4,C5,C6,C7,C8]),
-    maplist(wants_have_explained, [S01,S11,S21,S31,S41,S51,S61,S71,S81],[C0,C1,C2,C3,C4,C5,C6,C7,C8]),
+    maplist(can_explain,AllTeachers,AllConcepts),
+    maplist(wants_have_explained,AllFirstStudents,AllConcepts),
+    maplist(wants_have_explained,AllSecondStudents,AllConcepts),
 
-    labeling([ff],[T0,S00,S01,C0,T1,S10,S11,C1,T2,S20,S21,C2,T3,S30,S31,C3,T4,S40,S41,C4,T5,S50,S51,C5,T6,S60,S61,C6,T7,S70,S71,C7,T8,S80,S81,C8])
+    labeling([ff],Everything).
 
-
-
-.
-    
-header2conceptdict(Header,ConceptDict):- 
+% converting rows to dicts where keys are integers and values concepts/people    
+header_to_conceptdict(Header,ConceptDict):- 
     row_to_list(Header,[_|ConceptsList]),
     list_to_dict(ConceptsList,ConceptDict).
 
-rows2peopledict(Rows,PeopleDict):- 
+rows_to_peopledict(Rows,PeopleDict):- 
     rows_to_lists(Rows,RowsLL),
     transpose(RowsLL,[FR|_]),
     list_to_dict(FR,PeopleDict).
 
-explain(InPath,OutPath,OutRows) :-
-    % input -> internal representation done 
-     csv_read_file(InPath,[Header|InRows]),
-     header2conceptdict(Header,ConceptDict),
-     rows2peopledict(InRows,PeopleDict),
-    %  length(PeopleDict,PeopleCount),
-     length(ConceptDict,ConceptCount),
-     rows_to_rels(PeopleDict,ConceptDict,Header,InRows,RelationsUnfiltered),
-     filter_does_not_want(RelationsUnfiltered,Relations),
-     maplist(assertz,Relations),
-     rounds9(ConceptCount,Rounds),
-
+explain(InPath,OutPath,OutputRows) :-
+    % input -> internal representation 
+    csv_read_file(InPath,[Header|InRows]),
+    header_to_conceptdict(Header,ConceptDict),
+    rows_to_peopledict(InRows,PeopleDict),
+    % length(PeopleDict,PeopleCount), % this might be used for more people than 9
+    length(ConceptDict,ConceptCount),
+    rows_to_rels(PeopleDict,ConceptDict,Header,InRows,RelationsUnfiltered),
+    filter_does_not_want(RelationsUnfiltered,Relations),
+    maplist(assertz,Relations),
+    rounds9(ConceptCount,Rounds),
     % generated solution -> output csv
-    rounds_to_rows(PeopleDict,ConceptDict,Rounds,OutRows),
-     outheader(OutHeader),
-     csv_write_file(OutPath, [OutHeader|OutRows],[]).
-     
+    rounds_to_rows(PeopleDict,ConceptDict,Rounds,OutputRows),
+    outheader(OutHeader),
+    csv_write_file(OutPath, [OutHeader|OutputRows],[]).
